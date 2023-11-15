cbfbf4327a9487c32cfc6ee9f55ccdfc772ca210
Mengying Li <54960312+Lmy9102@users.noreply.github.com>
2023-11-15 18:42:32 -0500

    Shiny App for Team Functional Expertise Diversity

---
+++ Shiny_App_Diversity/app.R
@@ -0,0 +1,63 @@
+
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+# Create a user interface with inputs for team member expertise and an output area for the plot.
+ui1 <- fluidPage(
+  titlePanel("Functional Expertise Diversity and Team Performance"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("member1", "Team Member 1", choices = c("Engineering", "Project Management", "Accounting", "Finance", "Computer Science")),
+      selectInput("member2", "Team Member 2", choices = c("Engineering", "Project Management", "Accounting", "Finance", "Computer Science")),
+      selectInput("member3", "Team Member 3", choices = c("Engineering", "Project Management", "Accounting", "Finance", "Computer Science")),
+      selectInput("member4", "Team Member 4", choices = c("Engineering", "Project Management", "Accounting", "Finance", "Computer Science")),
+      selectInput("member5", "Team Member 5", choices = c("Engineering", "Project Management", "Accounting", "Finance", "Computer Science"))
+    ),
+    mainPanel(plotlyOutput("performancePlot"))
+  )
+)
+
+
+# Set up the server logic to process input and generate the plot.
+server1 <- function(input, output) {
+  
+  # Reactive data frame to store the history of team composition
+  history <- reactiveVal(data.frame(Diversity = numeric(), Performance = numeric(), Composition = character()))
+  
+  # Update history whenever inputs change
+  observeEvent(list(input$member1, input$member2, input$member3, input$member4, input$member5), {
+    team <- c(input$member1, input$member2, input$member3, input$member4, input$member5)
+    diversity <- length(unique(team))
+    
+    # Use a logarithmic function for performance calculation
+    performance <- 50 * log1p(diversity - 1)
+    composition <- paste(sort(unique(team)), collapse = ", ")
+    
+    # Update the history
+    new_history <- rbind(history(), data.frame(Diversity = diversity, Performance = performance, Composition = composition))
+    history(new_history)
+  })
+  
+  # Generate the plot with a dotted line
+  output$performancePlot <- renderPlotly({
+    data <- history()
+    
+    # Create ggplot
+    p <- ggplot(data, aes(x = Diversity, y = Performance, group = 1)) +
+      geom_line(aes(text = Composition), linetype = "dotted") +
+      geom_point(aes(text = Composition)) +
+      labs(title = "Team Performance vs Functional Expertise Diversity",
+           x = "Diversity of Functional Expertise",
+           y = "Team Performance") +
+      theme_minimal()
+    
+    # Convert to plotly
+    ggplotly(p, tooltip = "text")
+  })
+}
+
+
+
+# Run the application 
+shinyApp(ui = ui1, server = server1)
